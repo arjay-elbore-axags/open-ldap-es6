89eeb7eed90cafa8e4d4eafb6cac41720186a61a
Dev HK <devhk@Dev-at-Work.local>
2016-03-15 17:19:13 +0800

    add gulp task for test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,10 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+
+var mocha = require('gulp-mocha');
+var util = require('gulp-util');
+var babel_reg = require('babel/register');
  
 gulp.task('default', function() {
 	return gulp.src('src/**/*.js')
@@ -12,4 +16,13 @@ gulp.task('default', function() {
 		.pipe(concat('all.js'))
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function () {
+    return gulp.src(['test/**/*.js'], { read: false })
+        .pipe(mocha({ 
+            reporter: 'spec',
+            compilers: { js: babel_reg } 
+        }))
+        .on('error', util.log);
 });
